f26f55fbea5af5166552a1ec51ffddae0de7a0aa
Sawyer Matteson <stmatteson1@buffs.wtamu.edu>
2022-09-23 14:54:13 -0500

    Migrations for customer

---
+++ Pages/Customers/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Data;
+using DeliveryCart.Models;
+
+namespace DeliveryCart_Customer.Pages.Customers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Data.DeliveryCartContext _context;
+
+        public DeleteModel(Data.DeliveryCartContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Customer Customer { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Customer == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customer.FirstOrDefaultAsync(m => m.customerID == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Customer = customer;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Customer == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customer.FindAsync(id);
+
+            if (customer != null)
+            {
+                Customer = customer;
+                _context.Customer.Remove(Customer);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<DeliveryCartContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DeliveryCartContext") ?? throw new InvalidOperationException("Connection string 'DeliveryCartContext' not found.")));
 
 var app = builder.Build();
 
